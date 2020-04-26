8ed3b48232a1e76feb5aecaae00e1ba251496511
Yaroslav Hryhoruk <yaroslav.hryhoruk@onedome.com>
2020-04-27 02:32:46 +0300

    check to material-ui

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,42 +1,15 @@
 import React, {Component} from 'react';
 import {Switch, Route} from 'react-router-dom';
 
-import Videos from './Videos/Videos.jsx';
-import Shows from './Shows/Shows.jsx';
-import Promocodes from './Promocodes/Promocodes.jsx';
-import Campaigns from './Campaigns/Campaigns.jsx';
-import SidebarMenu from '../components/SidebarMenu/SidebarMenu.jsx';
-import PromocodeIcon from '../components/Icons/PromocodeIcon.jsx';
-
-import HeaderLayout from '../layouts/HeaderLayout/HeaderLayout.jsx';
+import HomePage from './HomePage';
 
 import './App.less';
 
 const ROUTES = [
     {
-        title: 'Videos',
-        path: '/videos',
-        icon: <PromocodeIcon/>,
-        page: <Videos/>
-    },
-    {
-        title: 'Shows',
-        path: '/shows',
-        icon: <PromocodeIcon/>,
-        page:  <Shows/>,
-        editableSubmenu: ['Nike Air Max', 'Nike Sport']
-    },
-    {
-        title: 'Promocode',
-        path: '/promocode',
-        icon: <PromocodeIcon/>,
-        page:  <Promocodes/>,
-    },
-    {
-        title: 'Campaigns',
-        path: '/campaigns',
-        icon: <PromocodeIcon/>,
-        page:  <Campaigns/>
+        title: 'Home',
+        path: '/',
+        page: <HomePage/>
     }
 ];
 
@@ -44,15 +17,12 @@ class App extends Component {
     render() {
         return (
             <div className='app'>
-                <SidebarMenu menu={ROUTES}/>
                 <Switch>
                     {ROUTES.map(({path, page}) => (
                         <Route 
                             key={path}
                             path={path}>
-                            <HeaderLayout>
-                                {page}
-                            </HeaderLayout>
+                            {page}
                         </Route>
                     ))}
                     <Route
